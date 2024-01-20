a2876ccfbded9e4447419003b68fec1061a5e523
Mohd Aman <mohdaman@Mohds-MacBook-Pro.local>
2024-01-21 01:20:22 +0530

    Update qualification without css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/about/About';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Skills />
       <Services />
+      <Qualification />
     </main>
     </>
   );
