71dddd574e58ebc7d6112f3d744a5b4d757929dd
Mohd Aman <mohdaman@Mohds-MacBook-Pro.local>
2024-01-22 16:09:22 +0530

    Update Social Media LInk

--- src/components/home/Social.js
+++ src/components/home/Social.js
@@ -3,16 +3,16 @@ import React from 'react'
 const Social = () => {
   return (
     <div className='home__social'>
-        <a className='home__social_icon' href='/' target='_blank'>
+        <a className='home__social_icon' href='https://www.instagram.com/mohammad_aman_92/'>
             <i className='uil uil-instagram'></i>
         </a>
 
-        <a className='home__social_icon' href='/' target='_blank'>
+        <a className='home__social_icon' href='https://www.youtube.com/@AmanTeach'>
             <i className='uil uil-youtube'></i>
         </a>
 
 
-        <a className='home__social_icon' href='/' target='_blank'>
+        <a className='home__social_icon' href='https://github.com/aman9267'>
             <i className='uil uil-github-alt'></i>
         </a>
     </div>
