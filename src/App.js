c06ce4d05c92e250435d8cbf11bec1a2ea68e05b
Mohd Aman <mohdaman@Mohds-MacBook-Pro.local>
2024-01-23 11:41:55 +0530

    contact Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Services from './components/services/Services';
 import Qualification from './components/qualification/Qualification';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Skills />
       <Services />
       <Qualification />
+      <Contact />
     </main>
     </>
   );
