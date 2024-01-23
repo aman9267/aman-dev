37240f580eaf4074f1954b5935c3e0774667d48c
Mohd Aman <mohdaman@Mohds-MacBook-Pro.local>
2024-01-23 18:01:46 +0530

    Update footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import './App.css';
-import Skills from './components/Skills/Skills';
-import About from './components/about/About';
-import Header from './components/header/Header';
-import Home from './components/home/Home';
-import Services from './components/services/Services';
-import Qualification from './components/qualification/Qualification';
-import Contact from './components/contact/Contact';
+import "./App.css";
+import Skills from "./components/Skills/Skills";
+import About from "./components/about/About";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Services from "./components/services/Services";
+import Qualification from "./components/qualification/Qualification";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
-    <Header />
-    <main>
-      <Home />
-      <About />
-      <Skills />
-      <Services />
-      <Qualification />
-      <Contact />
-    </main>
+      <Header />
+      <main>
+        <Home />
+        <About />
+        <Skills />
+        <Services />
+        <Qualification />
+        <Contact />
+        <Footer />
+      </main>
     </>
   );
 }
